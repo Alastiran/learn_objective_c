c7dbfb594d6cf49c9f06888a62c427f71223c728
Alastiran <sharps.justin1865@gmail.com>
2014-09-25 12:22:44 +0900

    Initial Commit

---
+++ j_learn_objective_c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  j_learn_objective_c
+//
+//  Created by Justin Sharps on 2014/09/25.
+//  Copyright (c) 2014年 Bardic Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
